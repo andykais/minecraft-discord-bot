bb68dc3ff3a286c0911c7d0aaa35e07dd8f65b4c
Andrew Kaiser <kaisea.rpi@gmail.com>
2023-11-25 18:54:16 -0500

    allow gateway-us-east1-d.discord.gg

--- launch.sh
+++ launch.sh
@@ -6,6 +6,6 @@
   --unstable \
   --allow-run=git,java \
   --allow-env=MODE,DISCORD_TOKEN,DISCORD_ACTIVITY_CHANNEL,DISCORD_MONITOR_CHANNEL \
-  --allow-net=discord.com,gateway.discord.gg \
+  --allow-net=discord.com,gateway.discord.gg,gateway-us-east1-d.discord.gg \
   --check \
   src/cli.ts
