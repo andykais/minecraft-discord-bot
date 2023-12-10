b4f788d54b3cf860cf4bb12a619dee232720cda3
Andrew Kaiser <kaisea.rpi@gmail.com>
2023-12-10 15:25:56 -0500

    typing fix

--- src/services/r2_backups.ts
+++ src/services/r2_backups.ts
@@ -96,17 +96,17 @@ class R2Backups extends Service {
       })
 
       const UPLOAD_CHUNK_SIZE = 1e6 * 20 // 20Mb chunks
-      let buffers = []
+      let buffers: Uint8Array[] = []
       let buffers_size = 0
       let part_number = 1
       const parts: { ETag: string; PartNumber: number }[] = []
 
-      function upload_part() {
+      const upload_part = async () => {
         const buffer = bytes.concat(...buffers)
         console.log(`Uploading part ${part_number} of size ${(buffer.length / 1e6).toFixed(2)}Mb`)
         buffers = []
         buffers_size = 0
-        const part_result = await this.#s3_client.uploadPart({
+        const part_result = await this.#s3_client!.uploadPart({
           Bucket: context.config.backup.r2!.bucket,
           Key: r2_key,
           UploadId: multipart.UploadId!,
